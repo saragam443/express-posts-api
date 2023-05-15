273694253cf788e5f14e36e5c699c45cd339f35f
saragam <git@saragam.com.np>
2023-05-15 18:48:46 +0545

    logger removed

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,10 @@
-const { logEvents } = require("./logEvents");
+// const { logEvents } = require("./logEvents");
 
 const errorHandler = (err, req, res, next) => {
-  logEvents(
-    `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
-    "errLog.log"
-  );
+  // logEvents(
+  //   `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
+  //   "errLog.log"
+  // );
   console.log(err.stack);
   const status = res.statusCode ? res.statusCode : 500;
   res.status(status);
