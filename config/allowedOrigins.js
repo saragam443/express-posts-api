9d328bce1af15f27db6b766d7f54a4675e71590b
saragam <git@saragam.com.np>
2023-05-18 20:35:16 +0545

    origin added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://reactblog-kb8a.onrender.com",
   "http://localhost:3000",
-  "https://saragam443.github.io/react-blog-app"
+  "https://saragam443.github.io"
 ];
 
 module.exports = allowedOrigins;
